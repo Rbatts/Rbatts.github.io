e63045ec7d419fd5168eed297c5d3bc78db7e260
RichBatten <32482377+Rbatts@users.noreply.github.com>
2018-06-22 09:46:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,39 +24,32 @@ function reset(){
 }
 
 function date_time(){
-	document.getElementById("date_time").innerHTML = Date();5
+	document.getElementById("date_time").innerHTML = Date();
 }
-	
-const API_KEY = "apiKey=tXSjreliE1V0n9OS7589OML48MU4m6EN"
+
+const API_KEY = "apikey=tXSjreliE1V0n9OS7589OML48MU4m6EN"
 
 const URL = "https://api.giphy.com/v1"
 
-const RANDOM_END_POINT = "/gifs/random?"
+const TRENDING = "/gifs/trending?"
 
-function getGif() {
-  // we create a new instance of an HTTP request
-  var request = new XMLHttpRequest();
-  //we get the text that the user has typed in
-  searchTerm = document.getElementById('input-box').value;
-  // we set up the url endpoint we want to reach
-  var searchQuery = "&tag=" + searchTerm;
-  var requestUrl = URL + RANDOM_END_POINT + API_KEY + searchQuery
-  
-  // we make the request
-  request.open('GET', requestUrl);
-  request.responseType = 'json';
-  request.send();
-
-  //we do something with the successful response
-  request.onload = function() {
-    var response = request.response;
-    var imageUrl  = response.data.image_url;
-    document.getElementById('gif').src = imageUrl
-  }
+function getGifs(){
+	
+	var request = new XMLHttpRequest();
+	
+	var requestUrl = URL + TRENDING + API_KEY + "&limit=10"
+	
+	request.open('GET', requestUrl);
+	request.responseType = 'json';
+	request.send();
+	
+	request.onload = function() {
+		var response = request.response;
+		for(var i = 0; i < 10; i++){
+			document.getElementById('gif' + (i + 1)).src = response.data[i].images.fixed_width.url;
+		}
+	}
+	
 }
 
-
-
-
-
-
+		
